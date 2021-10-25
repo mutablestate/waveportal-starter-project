e04b0130c95fcc49adbb24f7ebb861d2b926acd0
mutablestate <benwebdev@gmail.com>
2021-10-25 18:49:40 +0700

    custom(text): initial wave portal

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,45 @@
+<script>
+	import '../app.css';
+</script>
+
+<main>
+	<slot />
+</main>
+
+<footer>
+	<p>
+		visit <a href="https://buildspace.so">buildspace.so</a> to learn how to build a Web3 App with Solidity
+		+ Ethereum Smart Contracts.
+	</p>
+</footer>
+
+<style>
+	main {
+		flex: 1;
+		display: flex;
+		flex-direction: column;
+		padding: 1rem;
+		width: 100%;
+		max-width: 1024px;
+		margin: 0 auto;
+		box-sizing: border-box;
+	}
+
+	footer {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		padding: 40px;
+	}
+
+	footer a {
+		font-weight: bold;
+	}
+
+	@media (min-width: 480px) {
+		footer {
+			padding: 40px 0;
+		}
+	}
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,60 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script context="module">
+	export const prerender = true;
+</script>
+
+<script>
+	function handleWave() {}
+</script>
+
+<svelte:head>
+	<title>WavePortal</title>
+</svelte:head>
+
+<div class="mainContainer">
+	<div class="dataContainer">
+		<div class="header">👋 Hey there!</div>
+
+		<div class="bio">
+			<div>I used SvelteKit to build and Cloudflare to deploy my WavePortal!</div>
+			<div>Connect your Ethereum wallet and wave at me!</div>
+		</div>
+
+		<button class="waveButton" on:click={handleWave}>Wave at Me</button>
+	</div>
+</div>
+
+<style>
+	.mainContainer {
+		display: flex;
+		justify-content: center;
+		width: 100%;
+		margin-top: 64px;
+	}
+
+	.dataContainer {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		max-width: 600px;
+	}
+
+	.header {
+		text-align: center;
+		font-size: 32px;
+		font-weight: 600;
+	}
+
+	.bio {
+		text-align: center;
+		color: gray;
+		margin-top: 16px;
+	}
+
+	.waveButton {
+		cursor: pointer;
+		margin-top: 16px;
+		padding: 8px;
+		border: 0;
+		border-radius: 5px;
+	}
+</style>
