3564c7d9be30f99271ca4b83d79de00ca0281ede
mutablestate <benwebdev@gmail.com>
2021-10-25 18:58:46 +0700

    build: SvelteKit config for Cloudflare

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,9 @@
+import adapter from '@sveltejs/adapter-static';
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
+		adapter: adapter(),
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte'
 	}
